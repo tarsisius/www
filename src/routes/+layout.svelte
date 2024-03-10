6965969e444380ee4a10c0356b76732a5a553387
tarsisius <tarsisiushp@gmail.com>
2024-03-10 11:14:37 +0700

    optimize fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  import "../app.css"
+</script>
+
 <main>
   <slot />
 </main>
@@ -7,27 +11,6 @@
     --blue: #10a0a0;
     --black: #040304;
     --white: #f8f8f8;
-  }
-
-  :global(html) {
-    @font-face {
-      font-family: "JetBrainsMono";
-      src: url("$lib/assets/fonts/JetBrainsMono-Regular.ttf");
-      font-weight: 400;
-      font-display: swap;
-    }
-    @font-face {
-      font-family: "JetBrainsMono";
-      src: url("$lib/assets/fonts/JetBrainsMono-SemiBold.ttf");
-      font-weight: 600;
-      font-display: swap;
-    }
-    @font-face {
-      font-family: "JetBrainsMono";
-      src: url("$lib/assets/fonts/JetBrainsMono-Bold.ttf");
-      font-weight: 700;
-      font-display: swap;
-    }
   }
 
   :global(body) {
